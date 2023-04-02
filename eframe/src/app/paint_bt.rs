4d04784ba70d24aa2ea2ca70c216a152ce21a378
msakuta <masahiro.sakuta@gmail.com>
2023-04-02 15:53:33 +0900

    Show ports in nodes

--- eframe/src/app/paint_bt.rs
+++ eframe/src/app/paint_bt.rs
@@ -1,6 +1,6 @@
 use eframe::emath::RectTransform;
 use egui::{pos2, Color32, FontId, Frame, Painter, Pos2, Rect, Ui, Vec2};
-use swarm_rs::behavior_tree_lite::{parse_file, parser::TreeDef};
+use swarm_rs::behavior_tree_lite::{parse_file, parser::TreeDef, PortType};
 
 use crate::SwarmRsApp;
 
@@ -26,13 +26,7 @@ impl SwarmRsApp {
                 return;
             };
 
-            let font = FontId::monospace(16.);
-
-            let node_painter = NodePainter {
-                painter: &painter,
-                font,
-                to_screen: &to_screen,
-            };
+            let node_painter = NodePainter::new(&painter, &to_screen);
 
             node_painter.paint_node_recurse(NODE_PADDING, NODE_PADDING, &main.root());
         });
@@ -48,13 +42,24 @@ const NODE_SPACING: f32 = 20.;
 
 struct NodePainter<'p> {
     painter: &'p Painter,
-    font: FontId,
     to_screen: &'p RectTransform,
+    font: FontId,
+    port_font: FontId,
 }
 
 impl<'p> NodePainter<'p> {
-    fn paint_node_recurse(&self, mut x: f32, y: f32, node: &TreeDef<'_>) -> Vec2 {
+    fn new(painter: &'p Painter, to_screen: &'p RectTransform) -> Self {
+        Self {
+            painter,
+            to_screen,
+            font: FontId::monospace(16.),
+            port_font: FontId::monospace(12.),
+        }
+    }
+
+    fn paint_node_recurse(&self, mut x: f32, mut y: f32, node: &TreeDef<'_>) -> Vec2 {
         let initial_x = x;
+        let initial_y = y;
         let galley = self.painter.layout_no_wrap(
             node.get_type().to_string(),
             self.font.clone(),
@@ -80,13 +85,31 @@ impl<'p> NodePainter<'p> {
         let tree_width = size.x.max(x - initial_x - NODE_PADDING2 - NODE_SPACING);
         let node_left = initial_x + (tree_width - size.x) / 2.;
 
+        y += size.y + NODE_PADDING;
+        let ports: Vec<_> = node
+            .port_maps()
+            .iter()
+            .map(|port| {
+                let port_galley = self.painter.layout_no_wrap(
+                    port.node_port().to_string(),
+                    self.port_font.clone(),
+                    match port.get_type() {
+                        PortType::Input => Color32::from_rgb(255, 255, 127),
+                        PortType::Output => Color32::from_rgb(127, 255, 255),
+                        PortType::InOut => Color32::from_rgb(127, 255, 127),
+                    },
+                );
+                let port_height = port_galley.size().y;
+                let port = (port_galley, y);
+                y += port_height;
+                port
+            })
+            .collect();
+
         self.painter.rect(
             self.to_screen.transform_rect(Rect {
-                min: pos2(node_left, y),
-                max: pos2(
-                    node_left + NODE_PADDING2 + size.x,
-                    y + NODE_PADDING2 + size.y,
-                ),
+                min: pos2(node_left, initial_y),
+                max: pos2(node_left + NODE_PADDING2 + size.x, y + NODE_PADDING),
             }),
             0.,
             Color32::from_rgb(63, 63, 31),
@@ -95,13 +118,21 @@ impl<'p> NodePainter<'p> {
 
         self.painter.galley(
             self.to_screen
-                .transform_pos(pos2(node_left + NODE_PADDING, y + NODE_PADDING)),
+                .transform_pos(pos2(node_left + NODE_PADDING, initial_y + NODE_PADDING)),
             galley,
         );
 
+        for (port, y) in ports {
+            self.painter.galley(
+                self.to_screen
+                    .transform_pos(pos2(node_left + NODE_PADDING, y)),
+                port,
+            );
+        }
+
         let from = self
             .to_screen
-            .transform_pos(pos2(node_left + size.x / 2., y + size.y + NODE_PADDING2));
+            .transform_pos(pos2(node_left + size.x / 2., y + NODE_PADDING));
         for to in subnode_connectors {
             self.painter.line_segment([from, to], (2., Color32::YELLOW));
         }
